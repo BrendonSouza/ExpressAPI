c5e105f1a85100fc487a49a61905fb9fb82c438c
Brendon <brendonqueiros@hotmail.com>
2022-08-06 01:57:22 -0300

    :wrench: limpando main

--- src/repositories/UsersRepository.ts
+++
@@ -1,9 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-
-import { User } from "../models/User"
-//notação pra informar a entidade
-@EntityRepository(User)
-class UsersRepository extends Repository<User>{
-
-}
-export { UsersRepository }
